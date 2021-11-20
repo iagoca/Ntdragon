1089789b3de042d3d6ba89454a06148a9d31bc3a
iagoca <sousathiagocandido@gmail.com>
2021-11-19 21:49:38 -0400

    Fist commit

---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: '🔋Aguarde sou so 1 mizera.',
+			success: 'Nao deu dessa vez.',
+			wrongFormat: 'Tem algo de errado tente olha o menu de novo.',
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'Isso nao e stcker',
+				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+			},
+			only: {
+				group: 'SOMENTE EM GRUPOS',
+				admin: 'VC NAO E ADM NAO TEM PERMICAO PARA USAR ESSE COMANDO!',
+				premium: 'VC NAO E PREMIUM!',
+				owner: 'SOMENTE MEU DONO !',
+				Badmin: 'ME DE ADM E TENTE DE NOVO!',
+			}
+		}
